50f6ce62abbbec705a763b0d2bbeedb4ffac968e
ramana11k <ramana11k@gmail.com>
2023-12-14 17:48:24 +0530

    username and password with args

--- 05-variable.sh
+++ 05-variable.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-USERNAME=$1
-PASSWORD=$2
+echo "Enter your username"
 
-echo "Enter your username: $USERNAME
-echo "Enter your password: $PASSWORD
+read USERNAME #The value enter above will be automaticllay fetched by the USERNAME variable
+
+echo "Enter your password" 
+
+read PASSWORD #The value enter above will be automaticllay fetched by the PASSWORD variable
+
+echo "Your username is $USERNAME, and Your password is $PASSWORD"
+
 
 
