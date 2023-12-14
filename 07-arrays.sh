e5ab01699b472178d69c9d8ed566ba12124e399d
ramana11k <ramana11k@gmail.com>
2023-12-14 20:43:46 +0530

    arrays for fruits

---
+++ 07-arrays.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+FRUITS=("apples", "mango", "pinaple")
+
+echo "First value: ${FRUITS[0]}"
+
+echo "Second value: ${FRUITS[1]}"
+
+echo "Third value: ${FRUITS[2]}"
+
+echo "All Fruits: ${FRUITS[@]}"
