f2acbc3b4d1f70636e6701505af843f4b8429bea
ramana11k <ramana11k@gmail.com>
2023-12-14 21:38:10 +0530

    adding simple condition mod2

--- 08-conditions.sh
+++ 08-conditions.sh
@@ -2,9 +2,9 @@
 
 NUMBER=$1
 
-if [$NUMBER -gt 100]
+if [ $NUMBER -gt 100 ]
 then
-    echo "The given $NUMBER is greater than 100"
+    echo "The given no: $NUMBER is greater than 100"
 else
-    echo "The given $NUMBER is not greaer than 100"
+    echo "The given no: $NUMBER is not greaer than 100"
 fi
