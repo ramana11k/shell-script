705910391da8f7864b774fc4e463f6bbf424fd16
ramana11k <ramana11k@gmail.com>
2023-12-14 13:14:37 +0530

    added args in variables

---
+++ 04-varables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1: Hello $PERSON2, how are you"
+echo "$PERSON2: Hi $PERSON1, very good morning"
+echo "$PERSON1: How are you $PERSON2"
+echo "$PERSON2: I am good. How are you?"
