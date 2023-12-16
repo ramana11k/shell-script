9f13f84dabd699362414b1174af43eef0114010b
ramana11k <ramana11k@gmail.com>
2023-12-16 07:01:38 +0530

    modified install cmd

--- 09-Install-mysql.sh
+++ 09-Install-mysql.sh
@@ -2,13 +2,9 @@
 
 ID=$(id -u)
 
-R="e[31m"
-G="e[32m"
-N="e[0m"
-
 if [ $ID -ne 0 ]
     then    
-        echo "$R Error: $N Please connect as Root uesr to install mysql"
+        echo "Error: Please connect as Root uesr to install mysql"
         exit 1
     else    
         echo "You are a root user"
